5ce801c425fa036eae6aac0c3873e3b404605ed7
sourTaste000 <47074495+sourtaste000@users.noreply.github.com>
2020-10-06 20:41:57 -0700

    added try catch
    Signed-off-by: sourTaste000 <47074495+sourTaste000@users.noreply.github.com>

--- src/main/kotlin/commands/CalculatorCommand.kt
+++ src/main/kotlin/commands/CalculatorCommand.kt
@@ -1,6 +1,7 @@
 package commands
 
 import Command
+import Send.stackTrace
 import arg
 import doesLater
 import greedyString
@@ -12,10 +13,14 @@ object CalculatorCommand : Command("calc") {
         greedyString("input") {
             doesLater { context ->
                 val input: String = context arg "input"
-                val manager = ScriptEngineManager()
-                val engine = manager.getEngineByName("js")
-                val result = engine.eval(input)
-                message.channel.send(result.toString())
+                try {
+                    val manager = ScriptEngineManager()
+                    val engine = manager.getEngineByName("js")
+                    val result = engine.eval(input)
+                    message.channel.send(result.toString())
+                } catch(e: Exception){
+                    message.stackTrace(e)
+                }
             }
         }
     }
